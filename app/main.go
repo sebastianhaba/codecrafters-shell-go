7c5fc688ae766c3bbeb13eaf2fce013622f94d29
Sebastian Haba <sebastian@haba.net.pl>
2025-03-07 14:49:23 +0100

    Handle invalid command input in main function
    Added logic to read user input, trim whitespace, and display an error message for invalid commands. This improves user feedback and enhances the program's usability.

--- app/main.go
+++ app/main.go
@@ -4,11 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	cmd, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	fmt.Fprintf(os.Stdout, "%s: invalid command", strings.TrimSpace(cmd))
 }
